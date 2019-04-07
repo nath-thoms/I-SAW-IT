5ab1d400158b89a91fea0f6c8a6d43a93f508a84
nath <nath.chadwick@gmail.com>
2019-04-07 22:05:38 +0100

    added handleRowDetails function
    add handleRowDetails function to select chosen product.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Table } from 'react-materialize';
+import { Container } from 'react-bootstrap';
 import * as API from '../API';
 import Axios from 'axios';
 
@@ -7,97 +8,73 @@ class Products extends Component {
 
     
     state = {
-        products: []
+        products: [],
+        currentProduct: []
     }
     
 
-    // async componentDidMount() {
-    //     const response = await Axios.get('https://jkq0dchnp0.execute-api.eu-west-1.amazonaws.com/dev/get-json-data')
-    //     const products = response.data
-    //     this.setState({products: products}, () => console.log(response.data, 'res'))
-    // }
-
-
-    componentDidMount () {
-         Axios.get(`${"https://cors-anywhere.herokuapp.com/"}https://jkq0dchnp0.execute-api.eu-west-1.amazonaws.com/dev/get-json-data`).then(response => {
-               this.setState({
-                   products: response.data
-               })
-               
-            })
-       
+    async componentDidMount() {
+        const response = await Axios.get('https://jkq0dchnp0.execute-api.eu-west-1.amazonaws.com/dev/get-json-data')
+        const products = response.data.products
+        this.setState({products: products})
     }
-    
 
-
-
+    handleRowDetails = id => {
+      const filteredProduct =  this.state.products.filter(product => product.id === id)
+        this.setState({
+            currentProduct: filteredProduct
+        }, () => console.log(filteredProduct[0].title, 'filter'))
+    }
   render() {
 
     const {products} = this.state;
+    console.log(products);
 
-    
-    
       return (
 
         <div>
             This is the Products component.
 
             <div className="products-table">
-
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>
-                                Name
-                            </th>
-                            <th>
-                                Product Type
-                            </th>
-                            <th>
-                                Sizes
-                            </th>
-                            <th>
-                                Vendor
-                            </th>
-                            <th>
-                                Tags
-                            </th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                    
+                <Container>
+                    <Table>
+                        <thead>
                             <tr>
-
-                                {
-                                   
-                                    Object.keys(products).map(product => {
-                                       return  (
-                                        <div>
-                                       <td key={product.id}>{product.title}</td>
-                                       <td key={product.id}>{product.id}</td>
-                                       <td key={product.id}>{product.vendor}</td>
-                                       <td key={product.id}>{product.handle}</td>
-                                       </div>
-                                    )
-                                    })
-                                }
-                              
-                                {/* <td>hello</td>
-                            
-                            
-                                <td>hello</td>
-                            
-                            
-                                <td>hello</td>
-                            
-                            
-                                <td>hello</td> */}
+                                <th>
+                                    Name
+                                </th>
+                                <th>
+                                    Product Type
+                                </th>
+                                <th>
+                                    Sizes
+                                </th>
+                                <th>
+                                    Vendor
+                                </th>
+                                <th>
+                                    Tags
+                                </th>
                             </tr>
+                        </thead>
 
-                        
-                    </tbody>
-                </Table>
+                        <tbody>
+                                    {
+                                        products.map(product => (
+                                            
+                                            <tr key={product.id} onClick={() => this.handleRowDetails(product.id)}>
+                                                <td>{product.title}</td>
+                                                <td>{product.product_type}</td>
+                                                <td>{product.options[0].values.toString().split(' ')}</td>
+                                                <td>{product.vendor}</td>
+                                                <td>{product.tags.split(',').slice(4, 9)}</td>
+                                            </tr>
+                                            
+                                        ))
+                                    }  
+                        </tbody>
+                    </Table>
+                </Container>
             </div>
 
         </div>
